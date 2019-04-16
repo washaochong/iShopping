f1bb547e0a381b21e370eac72bf1a17de88bfc39
fanrenxiuxian <51xiuxian@sina.com>
2019-04-16 18:47:16 +0800

    add product model and admin::products routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'admin/products#index'
+
+  namespace :admin do
+    resource :products
+  end
 end
