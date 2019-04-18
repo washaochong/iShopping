f6ba500d2dc51d0c4d169f0ca37ac1a91e9db025
fanrenxiuxian <51xiuxian@sina.com>
2019-04-18 10:45:28 +0800

    implement edit/update action

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -18,9 +18,16 @@ class Admin::ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find params[:id]
   end
 
   def update
+    @product = Product.find params[:id]
+    if @product.update product_params
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
   end
 
   def destroy
