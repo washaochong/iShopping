cf2c85918e97a5b466a4fa56057490c6bb7227b8
fanrenxiuxian <51xiuxian@sina.com>
2019-05-11 13:37:54 +0800

    implement clear cart function

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,9 @@
 class CartsController < ApplicationController
   def index
   end
+
+  def destroy
+    current_cart.cart_items.delete_all
+    redirect_back(fallback_location: root_path)
+  end
 end
