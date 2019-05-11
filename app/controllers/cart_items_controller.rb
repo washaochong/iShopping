cdba1dcbd2f25b326e1ea8fb1c91ba5494a9b2d2
fanrenxiuxian <51xiuxian@sina.com>
2019-05-11 18:18:12 +0800

    can change cart_item quantity

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,7 +1,22 @@
 class CartItemsController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    cart_item = CartItem.find params[:id]
+    cart_item.update cart_item_params
+    redirect_back(fallback_location: root_path)
+  end
+
   def destroy
     cart_item = CartItem.find params[:id]
     cart_item.destroy
     redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
   end
 end
